89ae9feab91a15f47b918fdc075d0224ac58783e
Xavier Naude <xavier_naude@laposte.net>
2021-03-26 15:43:25 +0100

    Update delete path

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -100,7 +100,7 @@ class PinsController extends AbstractController
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods={"DELETE"})
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_delete", methods={"DELETE"})
      * @param Request $req
      * @param Pin $pin
      * @param EntityManagerInterface $em
