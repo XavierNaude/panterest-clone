9a6370e603a1c3b03724ecbe40069fe43a8116cb
Xavier Naude <xavier_naude@laposte.net>
2021-04-01 19:11:56 +0200

    rename gravatar method to gravatar url

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -226,7 +226,7 @@ class User implements UserInterface
         return $this;
     }
 
-    public function gravatar(?int $size=100)
+    public function getGravatarUrl(?int $size=100)
     {
         return sprintf('https://www.gravatar.com/avatar/%s/?s=%d',md5(strtolower(trim($this->getEmail()))),'?s='.$size);
     }
