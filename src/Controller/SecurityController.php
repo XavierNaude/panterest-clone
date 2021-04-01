7e5b0ae175b0dcacc4c78e0a9cffdef48912769b
Xavier Naude <xavier_naude@laposte.net>
2021-04-01 14:51:20 +0200

    Add message if the email is not verified

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -33,5 +37,28 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/send_back_email_verify", name="app_send_back_email_verify", methods={"GET","POST"})
+     */
+    public function send_back_email_verify(UserRepository $user, EmailVerifier $emailVerifier) : Response
+    {
+        $user = $this->getUser();
+        $send_email = true;
+
+        $emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            (new TemplatedEmail())
+                ->from(new Address(
+                    $this->getParameter('app.mail_from_address'),
+                    $this->getParameter('app.mail_from_name')))
+                ->to($user->getEmail())
+                ->subject('Please Confirm your Email')
+                ->htmlTemplate('emails/registration/confirmation.html.twig')
+        );
+
+        $this->addFlash('success','Le mail à bien été envoyé sur votre adresse mail');
+
+        return $this->redirectToRoute('app_home',compact('send_email'));
     }
 }
