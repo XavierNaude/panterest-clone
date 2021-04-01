097bc2fb5f17ec750a595d32f25d106e4fe38a3d
Xavier Naude <xavier_naude@laposte.net>
2021-04-01 11:34:35 +0200

    Add constraint to ResetPasswordRequestFormType and traduction message for user

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
@@ -16,8 +17,11 @@ class ResetPasswordRequestFormType extends AbstractType
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter your email',
+                        'message' => 'Merci de renseigner votre adresse Email',
                     ]),
+                    new Email([
+                        'message' => 'Merci de renseigner une adresse Email valide'
+                    ])
                 ],
             ])
         ;
